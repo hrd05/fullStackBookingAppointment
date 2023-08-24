ab52e9b87e238d7a683ab566c3053a8a103fd938
hrd05 <harshdunkhwal55@gmail.com>
2023-08-24 15:57:02 +0530

    added del button

--- formStore.js
+++ formStore.js
@@ -4,7 +4,7 @@ var emailInput = document.getElementById('email');
 // storing name input and email input in local storage
 
 var form = document.getElementById('my-form');
-
+var userList = document.getElementById('users');
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(e){
@@ -13,7 +13,23 @@ function onSubmit(e){
     if(nameInput.value === '' || emailInput.value === ''){
         alert('Please enter all fields');
     }
+
+    
+
     else{
+        //create li list
+     const li = document.createElement('li');
+
+     li.appendChild(document.createTextNode(`${nameInput.value}-${emailInput.value}`));
+     
+     var delBtn = document.createElement('button');     
+     delBtn.appendChild(document.createTextNode('Delete'));
+     delBtn.className= 'btnDel';
+
+     li.appendChild(delBtn);
+     userList.appendChild(li);
+
+     // storing user in form of objects
         var newUser = {
             name: nameInput.value,
             email: emailInput.value
@@ -30,6 +46,21 @@ function onSubmit(e){
 
         // Store the updated user array back to local storage
         localStorage.setItem('UserDetails', JSON.stringify(users));
+
+        // del btn event listener 
+        delBtn.addEventListener('click', function(){
+            // remove list 
+            userList.removeChild(li);
+            
+            //also remove list from local storage
+            const userIndex = users.indexOf(newUser);
+            if (userIndex !== -1) {
+                users.splice(userIndex, 1);
+                localStorage.setItem('UserDetails', JSON.stringify(users));
+            }
+
+        });
+        
     }
 
     nameInput.value='';
