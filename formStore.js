f314ef0801349217830d5a18a05603631731f2ed
hrd05 <harshdunkhwal55@gmail.com>
2023-08-24 16:38:40 +0530

    added edit btn

--- formStore.js
+++ formStore.js
@@ -1,69 +1,70 @@
 var nameInput = document.getElementById('name');
 var emailInput = document.getElementById('email');
 
-// storing name input and email input in local storage
-
 var form = document.getElementById('my-form');
 var userList = document.getElementById('users');
 form.addEventListener('submit', onSubmit);
 
-function onSubmit(e){
-    e.preventDefault()
+function onSubmit(e) {
+    e.preventDefault();
 
-    if(nameInput.value === '' || emailInput.value === ''){
+    if (nameInput.value === '' || emailInput.value === '') {
         alert('Please enter all fields');
-    }
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} - ${emailInput.value}`));
 
-    
+        var editBtn = document.createElement('button');
+        editBtn.appendChild(document.createTextNode('Edit'));
+        editBtn.className = 'editBtn';
+        li.appendChild(editBtn);
 
-    else{
-        //create li list
-     const li = document.createElement('li');
+        var delBtn = document.createElement('button');
+        delBtn.appendChild(document.createTextNode('Delete'));
+        delBtn.className = 'btnDel';
+        li.appendChild(delBtn);
 
-     li.appendChild(document.createTextNode(`${nameInput.value}-${emailInput.value}`));
-     
-     var delBtn = document.createElement('button');     
-     delBtn.appendChild(document.createTextNode('Delete'));
-     delBtn.className= 'btnDel';
+        userList.appendChild(li);
 
-     li.appendChild(delBtn);
-     userList.appendChild(li);
-
-     // storing user in form of objects
         var newUser = {
             name: nameInput.value,
             email: emailInput.value
-        }
-        var storedUsers = localStorage.getItem('UserDetails');
+        };
+
+        var storedUsers = localStorage.getItem(newUser.name);
         var users = [];
 
         if (storedUsers) {
             users = JSON.parse(storedUsers);
         }
 
-        // Add the new user to the array
         users.push(newUser);
 
-        // Store the updated user array back to local storage
-        localStorage.setItem('UserDetails', JSON.stringify(users));
+        localStorage.setItem(newUser.name, JSON.stringify(users));
 
-        // del btn event listener 
-        delBtn.addEventListener('click', function(){
-            // remove list 
+        editBtn.addEventListener('click', function() {
+            nameInput.value = newUser.name;
+            emailInput.value = newUser.email;
             userList.removeChild(li);
-            
-            //also remove list from local storage
+
             const userIndex = users.indexOf(newUser);
             if (userIndex !== -1) {
                 users.splice(userIndex, 1);
-                localStorage.setItem('UserDetails', JSON.stringify(users));
+                localStorage.setItem(newUser.name, JSON.stringify(users));
             }
-
         });
-        
+
+        delBtn.addEventListener('click', function() {
+            userList.removeChild(li);
+
+            const userIndex = users.indexOf(newUser);
+            if (userIndex !== -1) {
+                users.splice(userIndex, 1);
+                localStorage.setItem(newUser.name, JSON.stringify(users));
+            }
+        });
     }
 
-    nameInput.value='';
-    emailInput.value='';
-    
+    nameInput.value = '';
+    emailInput.value = '';
 }
