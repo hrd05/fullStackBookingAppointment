531055a776ab0fdcc7bbceb3fa4fa69f57990411
hrd05 <harshdunkhwal55@gmail.com>
2023-09-13 16:14:30 +0530

    storinginCLoud

---
+++ StoringCloud.js
@@ -0,0 +1,55 @@
+ 
+ function saveToLocalStorageEvent(event){
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+
+    const userDetails = {
+        name,
+        email 
+    }
+   // localStorage.setItem(email, JSON.stringify(userDetails));
+   axios.post("https://crudcrud.com/api/1e63f4158a234ffe934fca13e6497982/appointmentData", userDetails)
+   .then((res)=>{
+    showUserOnScreen(res.data)
+    console.log(res);
+   })
+   .catch(err=>console.log(err));
+    //showUserOnScreen(userDetails)
+ }
+
+ function showUserOnScreen(userDetails){
+    const parentElement = document.getElementById('users');
+
+    const childElement = document.createElement('li');
+    childElement.className = 'list-group-item';
+    childElement.appendChild(document.createTextNode(`Name: ${userDetails.name} , Email: ${userDetails.email}`));
+
+    const delbtn = document.createElement('button');
+    const editBtn = document.createElement('button');
+
+    delbtn.className = 'btn btn-danger btn-sm float-right delete';
+    editBtn.className = 'btn btn-sm btn-dark float-right';
+
+    delbtn.textContent = 'DELETE';
+    editBtn.textContent = 'EDIT';
+
+    childElement.appendChild(delbtn);
+    childElement.appendChild(editBtn);
+    parentElement.appendChild(childElement);
+
+     //del btn functionality
+     delbtn.onclick = () => {
+        //localStorage.removeItem(userDetails.description);
+        parentElement.removeChild(childElement);
+    }
+    
+    editBtn.onclick = () => {
+        //localStorage.removeItem(userDetails.description);
+        parentElement.removeChild(childElement);
+        document.getElementById('name').value = userDetails.name;
+        document.getElementById('email').value = userDetails.email;
+        
+    }
+ }
